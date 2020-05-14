440873e5d8a92abf5a28054a6f396efd41df3f68
Bolshevik35 <hongquan03051997@gmail.com>
2020-05-14 13:30:43 -0700

    fix bug

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require('knex');
 const db = knex({
 	client: 'pg',
 	connection: {
-		connectionString: process.env.DATABASE_U,
+		connectionString: process.env.DATABASE_URL,
 		ssl: true,
 	}
 });
